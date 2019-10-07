7b3748f6a760ef3ea4902c4e5de0a34d8de2ab49
Nicholas Vachon <nicholasvachon@hotmail.com>
2019-10-07 13:44:25 -0400

    Added data and adjusted Initial_test accordingly

--- Initial_test.py
+++ Initial_test.py
@@ -11,11 +11,41 @@ Just copied the FAST-BERT example from:
 @author: nicholas
 """
 
+from pathlib import Path
 import torch
-import apex
+# import apex
 
-from pytorch_pretrained_bert.tokenization import BertTokenizer
 
-from fast_bert.data import BertDataBunch
-from fast_bert.learner import BertLearner
-from fast_bert.metrics import accuracy
+from fast_bert.data_cls import BertDataBunch
+# from fast_bert.learner import BertLearner
+# from fast_bert.metrics import accuracy
+
+
+
+
+
+
+DATA_PATH = Path('./sample_data/multi_label_toxic_comments/data/')     # path for data files (train and val)
+LABEL_PATH = Path('./sample_data/multi_label_toxic_comments/label/')  # path for labels file
+MODEL_PATH=Path('../models/')    # path for model artifacts to be stored
+LOG_PATH=Path('../logs/')       # path for log files to be stored
+
+
+
+databunch = BertDataBunch(DATA_PATH, LABEL_PATH,
+                          tokenizer='bert-base-uncased',
+                          train_file='train.csv',
+                          val_file='val.csv',
+                          label_file='labels.csv',
+                          text_col='text',
+                          label_col=['toxic','severe_toxic','obscene','threat','insult','identity_hate'],
+                          batch_size_per_gpu=16,
+                          max_seq_length=512,
+                          multi_gpu=True,
+                          multi_label=True,
+                          model_type='bert')
+
+
+
+
+

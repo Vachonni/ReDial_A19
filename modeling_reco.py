Corrected loss_func to be softmax with BCE

modeling_reco.py
@@ -50,7 +50,7 @@ def loss_fct(logits, labels):
                 ratings_mask[i, itemid] = 1
    #     masked_ratings = ratings * ratings_mask
         
-        return BCEWithLogitsLoss()(logits * ratings_mask, ratings)        
+        return BCELoss()((logits * ratings_mask).softmax(dim=1), ratings)        
     # If not, use regular BCE
     else:
         return BCEWithLogitsLoss()(logits, labels.view(logits.shape))